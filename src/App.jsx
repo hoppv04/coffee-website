5eedd4a21eb0c9918d9014ae12b2a47d91f648e6
hoppv <phamhopxd1@gmail.com>
2024-07-29 20:26:51 +0700

    Buy Coffee Form Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
+import WhereToBuy from "./WhereToBuy/WhereToBuy";
 
 const App = () => {
   return (
     <div className="overflow-x-hidden">
       <Hero />
       <Services />
+      <WhereToBuy />
     </div>
   );
 };
